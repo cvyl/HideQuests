828f72743f95afbc29a23882b40df809351efa67
跨性别 <cvyl@pm.me>
2024-06-16 18:41:53 +0200

    fix: find the quest badge properly

--- HideQuests.plugin.js
+++ HideQuests.plugin.js
@@ -1,6 +1,6 @@
 /**
  * @name HideQuests
- * @version 1.0.1
+ * @version 1.0.2
  * @description Hides/Removes the "Quests" tab and quest badges in Discord.
  * @author Mikka
  * @authorId 390527881891151872
@@ -33,15 +33,22 @@ module.exports = class HideQuest {
     }
 
     hideQuestBadge() {
-        const badgeSelectors = [
-            '#my-account-tab > div > div:nth-child(1) > div[class^="children"] > div > div[class^="userInfo"] > div:nth-child(2) > div[class^="container"][class*="badgeList"] > div:nth-child(5)',
-            '[id^="popout_"] > div > div > div > div > div[class^="body"] > div[class^="container"] > div[class^="tags"][class*="biteSize"] > div[class^="container"] > div:nth-child(5)',
-            '[id^="popout_"] > div > div > div > div > div[class^="body"] > div[class^="container"] > div[class^="tags"][class*="biteSize"] > div[class^="container"] > div:nth-child(2)'
+        const badgeContainersSelectors = [
+            '#my-account-tab > div > div:nth-child(1) > div[class^="children"] > div > div[class^="userInfo"] > div:nth-child(2) > div[class^="container"][class*="badgeList"]',
+            '[id^="popout_"] > div > div > div > div > div[class^="body"] > div[class^="container"] > div[class^="tags"][class*="biteSize"] > div[class^="container"]'
         ];
 
-        badgeSelectors.forEach(selector => {
-            const badges = document.querySelectorAll(selector);
-            badges.forEach(badge => badge.remove());
+        badgeContainersSelectors.forEach(selector => {
+            const badgeContainers = document.querySelectorAll(selector);
+            badgeContainers.forEach(container => {
+                const lastChild = container.lastElementChild;
+                if (lastChild) {
+                    const link = lastChild.querySelector('a[href="https://discord.com/settings/inventory"]');
+                    if (link) {
+                        lastChild.remove();
+                    }
+                }
+            });
         });
     }
 }
