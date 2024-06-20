0ec7c295c778e1074390717eef5623aa148e5d48
跨性别 <cvyl@pm.me>
2024-06-20 17:16:39 +0200

    feat: rewrite using zerespluginlibrary

--- HideQuests.plugin.js
+++ HideQuests.plugin.js
@@ -1,6 +1,6 @@
 /**
  * @name HideQuests
- * @version 1.1.0
+ * @version 1.1.3
  * @description Hides/Removes the "Quests" tab and quest badges in Discord. Now with changelog and customizable settings.
  * @author Mikka
  * @authorId 390527881891151872
@@ -11,67 +11,118 @@
  * @updateUrl https://raw.githubusercontent.com/cvyl/HideQuests/main/HideQuests.plugin.js
  */
 
-module.exports = (() => {
-    const config = {
-        info: {
-            name: "HideQuests",
-            version: "1.1.0",
-            description: "Hides/Removes the 'Quests' tab and quest badges in Discord. Now with changelog and customizable settings.",
-        },
-        changelog: [
+const config = {
+    info: {
+        name: "HideQuests",
+        authors: [
             {
-                title: "Improvements",
-                type: "added",
-                items: [
-                    "Added settings to toggle hiding Quests tab and quest badges.",
-                    "Added a changelog section."
-                ]
+                name: "Mikka",
+                discord_id: "390527881891151872",
+                github_username: "cvyl",
             }
         ],
-        defaultConfig: [
-            {
-                type: "switch",
-                id: "hideQuestTab",
-                name: "Hide Quests Tab",
-                note: "Toggle to hide the Quests tab in the library.",
-                value: true
-            },
-            {
-                type: "switch",
-                id: "hideQuestBadge",
-                name: "Hide Quest Badges",
-                note: "Toggle to hide the quest badges in profiles.",
-                value: true
-            }
-        ]
-    };
-
-    return class HideQuests {
-        constructor() {
-            this.settings = BdApi.loadData(config.info.name, "settings") || {
-                hideQuestTab: true,
-                hideQuestBadge: true
-            };
+        version: "1.1.3",
+        description: "Hides/Removes the 'Quests' tab and quest badges in Discord. Now with changelog and customizable settings.",
+        github: "https://github.com/cvyl/HideQuests/blob/main/HideQuests.plugin.js",
+        github_raw: "https://raw.githubusercontent.com/cvyl/HideQuests/main/HideQuests.plugin.js"
+    },
+    changelog: [
+        {
+            title: "Partial Rewrite (1.1.X+) using ZeresPluginLibrary",
+            type: "added",
+            items: [
+                "Added support for ZeresPluginLibrary for better compatibility and performance.",
+                "Added settings panel to toggle hiding Quests tab and quest badges.",
+                "Older versions of the plugin will no longer be supported.",
+                "Please report any issues on the GitHub repository."
+            ]
+        },
+        {
+            title: "Settings Panel (1.1.0)",
+            type: "added",
+            items: [
+                "Added settings panel to toggle hiding Quests tab and quest badges individually."
+            ]
+        },
+        {
+            title: "Initial Release (1.0.0)",
+            type: "added",
+            items: [
+                "Hides the Quests tab in the library.",
+                "Hides the quest badges in profiles."
+            ]
         }
 
-        start() {
-            if (this.settings.hideQuestTab) this.hideQuests();
-            if (this.settings.hideQuestBadge) this.hideQuestBadge();
-            this.observer = new MutationObserver(() => {
-                if (this.settings.hideQuestTab) this.hideQuests();
-                if (this.settings.hideQuestBadge) this.hideQuestBadge();
-            });
+    ],
+    defaultConfig: [
+        {
+            type: "switch",
+            id: "hideQuestTab",
+            name: "Hide Quests Tab",
+            note: "Toggle to hide the Quests tab in the library.",
+            value: true
+        },
+        {
+            type: "switch",
+            id: "hideQuestBadge",
+            name: "Hide Quest Badges",
+            note: "Toggle to hide the quest badges in profiles.",
+            value: true
+        }
+    ]
+};
+
+module.exports = !global.ZeresPluginLibrary ? class {
+    constructor() { this._config = config; }
+    load() {
+        BdApi.showConfirmationModal("Library plugin is needed", 
+            `The library plugin needed for ${config.info.name} is missing. Please click Download Now to install it.`, {
+            confirmText: "Download",
+            cancelText: "Cancel",
+            onConfirm: () => {
+                require("request").get("https://rauenzi.github.io/BDPluginLibrary/release/0PluginLibrary.plugin.js", async (error, response, body) => {
+                    if (error) return require("electron").shell.openExternal("https://betterdiscord.app/Download?id=9");
+                    await require("fs").writeFile(require("path").join(BdApi.Plugins.folder, "0PluginLibrary.plugin.js"), body, () => BdApi.Plugins.reload(config.info.name));
+                });
+            }
+        });
+    }
+    start() { }
+    stop() { }
+} : (([Plugin, Library]) => {
+    const { Settings } = Library;
+
+    return class HideQuests extends Plugin {
+        constructor() {
+            super();
+            this.settings = this.loadSettings(config.defaultConfig);
+        }
+
+        onStart() {
+            this.hideElements();
+            this.observer = new MutationObserver(() => this.hideElements());
             this.observer.observe(document.body, { childList: true, subtree: true });
         }
 
-        stop() {
+        onStop() {
             this.observer.disconnect();
+            this.resetElements();
+        }
+
+        hideElements() {
+            if (this.settings.hideQuestTab) this.hideQuests();
+            if (this.settings.hideQuestBadge) this.hideQuestBadge();
+        }
+
+        resetElements() {
+            this.resetQuests();
+            this.resetQuestBadge();
         }
 
         hideQuests() {
             const questContainer = document.querySelector('#library-inventory-tab > div[class^="questsContainer"]');
             if (questContainer) {
-                questContainer.setAttribute('hidden', 'true');
+                questContainer.style.display = 'none';
             }
         }
 
@@ -88,7 +139,34 @@ module.exports = (() => {
                     if (lastChild) {
                         const link = lastChild.querySelector('a[href="https://discord.com/settings/inventory"]');
                         if (link) {
-                            lastChild.setAttribute('hidden', 'true');
+                            lastChild.style.display = 'none';
+                        }
+                    }
+                });
+            });
+        }
+
+        resetQuests() {
+            const questContainer = document.querySelector('#library-inventory-tab > div[class^="questsContainer"]');
+            if (questContainer) {
+                questContainer.style.display = '';
+            }
+        }
+
+        resetQuestBadge() {
+            const badgeContainersSelectors = [
+                '#my-account-tab > div > div:nth-child(1) > div[class^="children"] > div > div[class^="userInfo"] > div:nth-child(2) > div[class^="container"][class*="badgeList"]',
+                '[id^="popout_"] > div > div > div > div > div[class^="body"] > div[class^="container"] > div[class^="tags"][class*="biteSize"] > div[class^="container"]'
+            ];
+
+            badgeContainersSelectors.forEach(selector => {
+                const badgeContainers = document.querySelectorAll(selector);
+                badgeContainers.forEach(container => {
+                    const lastChild = container.lastElementChild;
+                    if (lastChild) {
+                        const link = lastChild.querySelector('a[href="https://discord.com/settings/inventory"]');
+                        if (link) {
+                            lastChild.style.display = '';
                         }
                     }
                 });
@@ -96,58 +174,14 @@ module.exports = (() => {
         }
 
         getSettingsPanel() {
-            const panel = document.createElement("div");
-            panel.style.padding = "10px";
-
-            config.defaultConfig.forEach(setting => {
-                const container = document.createElement("div");
-                container.style.marginBottom = "10px";
-
-                const label = document.createElement("label");
-                label.innerText = setting.name;
-                label.style.display = "block";
-                label.style.marginBottom = "5px";
-
-                const note = document.createElement("small");
-                note.innerText = setting.note;
-                note.style.display = "block";
-                note.style.marginBottom = "5px";
-
-                const input = document.createElement("input");
-                input.type = "checkbox";
-                input.checked = this.settings[setting.id];
-                input.onchange = () => {
-                    this.settings[setting.id] = input.checked;
-                    BdApi.saveData(config.info.name, "settings", this.settings);
-                    this.stop();
-                    this.start();
-                };
-
-                container.appendChild(label);
-                container.appendChild(note);
-                container.appendChild(input);
-                panel.appendChild(container);
+            const panel = this.buildSettingsPanel();
+            panel.addListener((id, value) => {
+                this.settings[id] = value;
+                this.saveSettings(this.settings);
+                this.stop();
+                this.start();
             });
-
-            const changelogTitle = document.createElement("h3");
-            changelogTitle.innerText = "Changelog";
-            panel.appendChild(changelogTitle);
-
-            config.changelog.forEach(change => {
-                const changeTitle = document.createElement("h4");
-                changeTitle.innerText = `${change.title} (${change.type})`;
-                panel.appendChild(changeTitle);
-
-                const changeList = document.createElement("ul");
-                change.items.forEach(item => {
-                    const listItem = document.createElement("li");
-                    listItem.innerText = item;
-                    changeList.appendChild(listItem);
-                });
-                panel.appendChild(changeList);
-            });
-
-            return panel;
+            return panel.getElement();
         }
     };
-})();
+})(global.ZeresPluginLibrary.buildPlugin(config));
